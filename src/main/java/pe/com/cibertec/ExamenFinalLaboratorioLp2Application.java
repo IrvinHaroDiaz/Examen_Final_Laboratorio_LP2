7fc7ae0d1e36cc53d57212852f7973da6b5bf764
Irvin D. Haro Diaz <i202334997@cibertec.edu.pe>
2024-10-23 22:05:34 -0500

    estructura inicial de examen_t2

---
+++ src/main/java/pe/com/cibertec/ExamenFinalLaboratorioLp2Application.java
@@ -0,0 +1,13 @@
+package pe.com.cibertec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenFinalLaboratorioLp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenFinalLaboratorioLp2Application.class, args);
+	}
+
+}
